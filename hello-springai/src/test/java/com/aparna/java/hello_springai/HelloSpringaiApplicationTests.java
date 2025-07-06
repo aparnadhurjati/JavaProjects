0baf9ca1e09942193a92aab55fc0a27035cf4f86
aparnadhurjati <aparnasudha@gmail.com>
2025-07-06 14:07:51 -0400

    initial version

---
+++ hello-springai/src/test/java/com/aparna/java/hello_springai/HelloSpringaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aparna.java.hello_springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
